2a63a3a30f2ed6080fc21dca30c2a4d428f3f09f
Piotr Bandurski <redroy44@gmail.com>
2017-01-09 23:14:13 +0100

    project refresh and first plot

--- analysisR.R
+++ analysisR.R
@@ -1,6 +1,18 @@
+# Load the packages -------------------------------------------------------
+library(checkpoint)
+checkpoint("2016-11-15")
 library(tidyverse)
 library(lubridate)
 library(stringr)
+library(rvest)
+library(tidytext)
+library(wordcloud)
+library(doMC)
+registerDoMC(cores = 8)
+set.seed(29082012)
+
+
+# Load the data -----------------------------------------------------------
 
 url <- "http://files.grouplens.org/datasets/movielens/"
 dataset_small <- "ml-latest-small"
@@ -18,35 +30,63 @@ if (!file.exists(file.path(data_folder, dataset_zip))) {
 }
 unzip(file.path(data_folder, dataset_zip), exdir = data_folder)
 
-# Load the data
+# Load the files
 dataset_files <- c("movies", "ratings", "links", "tags")
 suffix <- ".csv"
 
 for (f in dataset_files) {
   path <- file.path(data_folder, dataset, paste0(f, suffix))
-  assign(f, read.csv(path, header = T, stringsAsFactors = F))
-  print(object.size(f),units="Gb")
+  assign(f, read_csv(path))
+  print(object.size(f),units="Mb") # Not working?
 }
 
+
+# Data cleaning -----------------------------------------------------------
+
 # Clean ratings
-ratings <- tbl_df(ratings) %>%
+ratings_df <- tbl_df(ratings) %>%
   mutate(rating = as.factor(rating), timestamp = as_datetime(timestamp))
 
 # Clean movies
-# TODO (no genres listed) -> NA
-movies <- tbl_df(movies)
-movies <-movies %>%
+movies_df <- tbl_df(movies)
+movies_df <- movies_df %>%
   mutate(title = str_trim(title)) %>% # trim whitespaces
   extract(title, c("title_tmp", "year"), regex = "^(.*) \\(([0-9 \\-]*)\\)$", remove = F) %>% # split title to title, year
-  mutate(year = ifelse(str_length(year) > 4, as.integer(str_split(year, "-", simplify = T)[1]), as.integer(year))) %>% # for series take debut date
-  mutate(title = ifelse(is.na(title_tmp), title, title_tmp)) %>% # replace title NA's with original title
-  select(-title_tmp) # drop title1 column
+  mutate(year = if_else(str_length(year) > 4, as.integer(str_split(year, "-", simplify = T)[1]), as.integer(year))) %>% # for series take debut date
+  mutate(title = if_else(is.na(title_tmp), title, title_tmp)) %>% # replace title NA's with original title
+  select(-title_tmp)  %>%# drop title1 column
+  mutate(genres = if_else(genres == "(no genres listed)", `is.na<-`(genres), genres)) # generic function to turn (no genres listed) to NA
 
 # Check NA's
-na_movies <- movies %>%
+na_movies <- movies_df %>%
   filter(is.na(title) | is.na(year)) %>%
   print
 
 # Clean tags
-tags <- tbl_df(tags) %>%
+tags_df <- tbl_df(tags) %>%
   mutate(timestamp = as_datetime(timestamp))
+
+# Genres popularity per year
+genres_popularity <- movies_df %>%
+  na.omit() %>%
+  select(movieId, year, genres) %>%
+  separate_rows(genres, sep = "\\|") %>%
+  group_by(year, genres) %>%
+  summarise(number = n()) 
+
+genres_popularity %>%
+  filter(year > 1950) %>%
+  ggplot(aes(x = year, y = number)) +
+    geom_line(aes(group=genres, color=genres))
+  
+
+
+
+
+
+
+
+
+
+
+
